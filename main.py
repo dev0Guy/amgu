b0c6efe473cc519075f0a12d9b2d05137418dc8c
Guy Arieli <97923827+dev0Guy@users.noreply.github.com>
2022-05-23 12:56:27 +0300

    main - runner
    example of using the package.

---
+++ main.py
@@ -0,0 +1,38 @@
+from amgu.premade.cityflow import  DiscreteCF,AvgWaitingTime,CNN,RayRunner,vanila
+
+
+config = {
+    'env_config':{
+        'config_path': "examples/hangzhou_1x1_bc-tyc_18041607_1h/config.json",
+        "steps_per_episode": 100,
+        'res_path': 'res/',
+    },
+    'stop': {"training_iteration": 5},
+    'res_path': 'res/',
+    'framework': "torch",
+    'seed': 123,
+    'evaluation_interval': 10,
+    'evaluation_duration': 5,
+    'exploration_config': {
+        'type': "EpsilonGreedy",
+        'epsilon_schedule': {
+            'type': "ExponentialSchedule",
+            'initial_p': 1,
+            'schedule_timesteps': 100//5,
+            'decay_rate': 0.99,
+        }
+    },
+    'model': {
+        'custom_model': 'new_models',
+        'custom_model_config': {
+            'intersection_num': 1,
+            'hidden_size': 10,
+        }
+    },
+    'run_from': '/Users/guyarieli/Documents/GitHub/amgu/',
+    'env' : 'custom_env',
+}
+preprocess_dict = {'func': vanila,'argument_list':[]}
+env_func = lambda _: DiscreteCF(config['env_config'],AvgWaitingTime,preprocess_dict)
+runner = RayRunner(config,CNN,env_func,'DQN')
+runner.train()
